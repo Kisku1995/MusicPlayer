75f93651b3885048a74697dcbbe77ef89d93c043
dns21395 <dns21395@gmail.com>
2017-07-15 22:51:35 +0900

    probably mediaplayer singleton cannot live between service and activity

--- MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/fragments/tracks/TracksAdapter.kt
+++ MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/fragments/tracks/TracksAdapter.kt
@@ -1,11 +1,14 @@
 package gabyshev.denis.musicplayer.fragments.tracks
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import gabyshev.denis.musicplayer.R
+import gabyshev.denis.musicplayer.service.MediaPlayerService
 import gabyshev.denis.musicplayer.service.MusicMediaPlayer
 import gabyshev.denis.musicplayer.service.TrackData
 
@@ -13,24 +16,30 @@ import gabyshev.denis.musicplayer.service.TrackData
  * Created by Borya on 15.07.2017.
  */
 
-class TracksAdapter(context: Context, arrayTracks: ArrayList<TrackData>): RecyclerView.Adapter<TracksHolder>() {
-    private val mArrayTracks: ArrayList<TrackData> = arrayTracks
-    private val mContext: Context = context
+class TracksAdapter(private val context: Context, private val arrayTracks: ArrayList<TrackData>): RecyclerView.Adapter<TracksHolder>() {
+    private val TAG = "TracksAdapter"
 
     init {
-        MusicMediaPlayer.instance()!!.setPlaylist(mArrayTracks)
+        MusicMediaPlayer.instance()!!.setPlaylist(arrayTracks)
     }
 
-    override fun getItemCount(): Int = mArrayTracks.size
+    override fun getItemCount(): Int = arrayTracks.size
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TracksHolder {
-        return TracksHolder(LayoutInflater.from(mContext).inflate(R.layout.fragment_tracks_item, parent, false))
+        return TracksHolder(LayoutInflater.from(context).inflate(R.layout.fragment_tracks_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: TracksHolder?, position: Int) {
-        holder?.bindTracksHolder(mContext, mArrayTracks[position], position)
+        holder?.bindTracksHolder(context, arrayTracks[position], position)
         holder?.itemView?.setOnClickListener {
-            MusicMediaPlayer.instance()!!.setActiveAudioAndPlay(position)
+            if(!MediaPlayerService.isRunning(context, MediaPlayerService::class.java)) {
+                Log.d(TAG, "service not running")
+                context.startService(Intent(context, MediaPlayerService::class.java))
+                MusicMediaPlayer.instance()!!.setActiveAudioAndPlay(position)
+            } else {
+                Log.d(TAG, "service running")
+                MusicMediaPlayer.instance()!!.setActiveAudioAndPlay(position)
+            }
         }
     }
 
--- MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/service/MediaPlayerService.kt
+++ MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/service/MediaPlayerService.kt
@@ -1,6 +1,8 @@
 package gabyshev.denis.musicplayer.service
 
+import android.app.ActivityManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
@@ -9,16 +11,29 @@ import android.os.IBinder
  * Created by Borya on 15.07.2017.
  */
 
-class MediaPlayerService : Service() {
-
-
-    init {
-
+class MediaPlayerService: Service() {
+    companion object {
+        fun isRunning(context: Context, serviceClass: Class<*>): Boolean {
+            val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+            for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+                if (serviceClass.getName().equals(service.service.className)) {
+                    return true
+                }
+            }
+            return false
+        }
     }
 
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        MusicMediaPlayer.instance()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+
 
 
 }
