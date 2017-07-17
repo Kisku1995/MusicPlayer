488e29454ee2a1dab85f6c3445ac42101dde9e8b
dns21395 <dns21395@gmail.com>
2017-07-17 19:25:28 +0900

    added tablayout

--- MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/MainActivity.kt
+++ MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/MainActivity.kt
@@ -2,44 +2,17 @@ package gabyshev.denis.musicplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.view.ViewPager
-import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import gabyshev.denis.musicplayer.fragments.PlayerViewPagerAdapter
-import gabyshev.denis.musicplayer.fragments.RxMenuViewPager
-import gabyshev.denis.musicplayer.fragments.menu.MenuAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var menuLayoutManager: LinearLayoutManager
-    private lateinit var adapter: MenuAdapter
     private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        menuLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        menu.layoutManager = menuLayoutManager
-        adapter = MenuAdapter(this, menuLayoutManager)
-        menu.adapter = adapter
-
         viewPager.adapter = PlayerViewPagerAdapter(supportFragmentManager);
-        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrollStateChanged(state: Int) {
-            }
-
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-            }
-
-            override fun onPageSelected(position: Int) {
-                Log.d(TAG, "onPageSelected : ${position}")
-                adapter.setSelectedHolder(position)
-            }
-        }
-
-        )
-
-
+        menu.setupWithViewPager(viewPager)
     }
 }
--- MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/fragments/PlayerViewPagerAdapter.kt
+++ MusicPlayer/app/src/main/java/gabyshev/denis/musicplayer/fragments/PlayerViewPagerAdapter.kt
@@ -10,12 +10,17 @@ import gabyshev.denis.musicplayer.fragments.tracks.TracksFragment
  * Created by Borya on 15.07.2017.
  */
 class PlayerViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+    private val menuItems = arrayOf("tracks", "playlists", "albums", "artists", "genres")
 
     override fun getItem(position: Int): Fragment {
         if(position == 0)
         return TracksFragment.instance()!!
         else
             return PlaylistsFragment()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return menuItems[position]
     }
 
     override fun getCount(): Int = 5
